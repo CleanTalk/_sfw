645937e339e51a6a45903561aa6df4be3a26bead
Glomberg <bazz@bk.ru>
2023-01-31 19:17:28 +0300

    Fix. FW module ipAppendAdditional method fixed.

--- FirewallModule.php
+++ FirewallModule.php
@@ -13,6 +13,7 @@ namespace Cleantalk\Common\Firewall;
  * @since 2.49
  */
 
+use Cleantalk\Common\Helper\Helper;
 use Cleantalk\Common\Mloader\Mloader;
 use Cleantalk\Common\Variables\Get;
 
@@ -125,7 +126,9 @@ abstract class FirewallModule {
     public function ipAppendAdditional(& $ips)
 	{
 		$this->real_ip = isset($ips['real']) ? $ips['real'] : null;
-        $helper_class = $this->helper;
+
+        /** @var Helper $helper_class */
+        $helper_class = Mloader::get('Helper');
 
 		if( Get::get('sfw_test_ip') && $helper_class::ipValidate( Get::get('sfw_test_ip') ) !== false ) {
             $this->ip_array['sfw_test'] = Get::get( 'sfw_test_ip' );
